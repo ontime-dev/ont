e317aa960e39f6b4d5a57fb9ca7880c08543aa68
HassanAlsamahi <hassanalsamahi12@gmail.com>
2024-03-05 00:43:37 +0100

    Implement ProcessTable func

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,8 +2,10 @@ package service
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+	"ont/internal/dbopts"
+	"sync"
+	"time"
 )
 
 func Letsgo(logger *log.Logger) error {
@@ -14,22 +16,48 @@ func Letsgo(logger *log.Logger) error {
 	}
 
 	defer db.Close()
-	cmd := fmt.Sprintf("SHOW TABLES;")
+
 	for {
 		var tablename string
-		tables, err := db.Query(cmd)
+		var allTables []string
+		tables, err := db.Query("SHOW TABLES;")
 		if err != nil {
 			logger.Fatal(err)
 		}
-		go func() {
-			for tables.Next() {
-				err = tables.Scan(&tablename)
-				if err != nil {
-					logger.Fatal(err.Error())
-				}
-				logger.Print(tablename)
+		for tables.Next() {
+			err = tables.Scan(&tablename)
+			if err != nil {
+				logger.Fatal(err.Error())
 			}
-		}()
+			allTables = append(allTables, tablename)
+		}
+		var wg sync.WaitGroup
+		for _, table := range allTables {
+			wg.Add(1)
+			go ProcessTable(db, table, logger, &wg)
+		}
+		wg.Wait()
+	}
+}
+
+func ProcessTable(db *sql.DB, table string, logger *log.Logger, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var job dbopts.Jobs
+
+	maxID, err := dbopts.GetMaxID(db, table)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Println(table)
+	for id := 1; id <= maxID; id++ {
+		job, err := dbopts.GetJob(db, table, id, job)
+		logger.Print(job.Id)
+		if err != nil {
+			logger.Fatal(err)
+		}
+		if job.Exec_time == time.Now().Format("15:04:05 Jan 02 2006") {
+			logger.Println("Execute")
+		}
 
 	}
 }
