22c0051a310db17bcf2119b196d646e511f49451
HassanAlsamahi <hassanalsamahi12@gmail.com>
2024-03-10 14:43:29 +0100

    Add now+value in from flag

--- internal/run/run.go
+++ internal/run/run.go
@@ -53,8 +53,20 @@ func ParseFrom(from string) (time.Time, error) {
 			from = from + " " + crntTime.Format("15:04:05")
 		} else if strings.Contains(from, ":") {
 			from = crntTime.Format("02-01-2006") + " " + from
+		} else if strings.Contains(from, "+") {
+			from_string := strings.Split(from, "+")
+			//from := from_string[0]
+			num, last_char := GetLastChar(from_string[1])
+			from, err = ParseEvery(crntTime, last_char, num)
+			if err != nil {
+				return crntTime, err
+			}
+			crntTime, err = time.Parse("15:04:05 Jan 02 2006", from)
+			if err != nil {
+				return crntTime, err
+			}
+			return crntTime, nil
 		}
-
 		crntTime, err = time.Parse("02-01-2006 15:04:05", from)
 		if err != nil {
 			return crntTime, err
@@ -68,28 +80,22 @@ func ParseEvery(crntTime time.Time, last_char string, number int) (string, error
 
 	switch last_char {
 	case "h":
-		fmt.Println("every hour")
 		next_run := crntTime.Add(time.Hour * time.Duration(number)).Format("15:04:05 Jan 02 2006")
 		return next_run, nil
 	case "m":
-		fmt.Println("every minute")
 		next_run := crntTime.Add(time.Minute * time.Duration(number)).Format("15:04:05 Jan 02 2006")
 		return next_run, nil
 	case "s":
-		fmt.Println("every seconds")
 		next_run := crntTime.Add(time.Second * time.Duration(number)).Format("15:04:05 Jan 02 2006")
 		return next_run, nil
 	case "d":
-		fmt.Println("every day")
 		next_run := crntTime.AddDate(0, 0, number).Format("15:04:05 Jan 02 2006")
 		return next_run, nil
 	case "w":
-		fmt.Println("every week")
 		next_run := crntTime.AddDate(0, 0, number*7).Format("15:04:05 Jan 02 2006")
 		return next_run, nil
 	case "M":
 		next_run := crntTime.AddDate(0, number, 0).Format("15:04:05 Jan 02 2006")
-		fmt.Println("every month")
 		return next_run, nil
 	case "y":
 		next_run := crntTime.AddDate(number, 0, 0).Format("15:04:05 Jan 02 2006")
