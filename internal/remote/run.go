d7240352871b0caa104c202ff3a30c4f876afbd1
HassanAlsamahi <hassanalsamahi12@gmail.com>
2024-07-29 14:27:50 +0200

    reform the code of remote.Run

--- internal/remote/run.go
+++ internal/remote/run.go
@@ -1,107 +1,98 @@
 package remote
 
 import (
-	"errors"
 	"fmt"
 	"net"
-	"ont/internal/escape"
 	"os"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/knownhosts"
 )
 
-func addHostKey(host, user string, remote net.Addr, pubKey ssh.PublicKey) error {
-	// add host key if host is not found in known_hosts, error object is return, if nil then connection proceeds,
-	// if not nil then connection stops.
-	//khFilePath := filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts")
-	knownHostsFilepath := fmt.Sprintf("/home/%s/.ssh/known_hosts", user)
+// func addHostKey(host, user string, remote net.Addr, pubKey ssh.PublicKey) error {
+// 	// add host key if host is not found in known_hosts, error object is return, if nil then connection proceeds,
+// 	// if not nil then connection stops.
+// 	//khFilePath := filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts")
+// 	knownHostsFilepath := fmt.Sprintf("/home/%s/.ssh/known_hosts", user)
 
-	file, err := os.OpenFile(knownHostsFilepath, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		escape.LogPrint(err.Error())
-	}
-	defer file.Close()
+// 	file, err := os.OpenFile(knownHostsFilepath, os.O_APPEND|os.O_WRONLY, 0600)
+// 	if err != nil {
+// 		escape.LogPrint(err.Error())
+// 	}
+// 	defer file.Close()
 
-	knownHosts := knownhosts.Normalize(remote.String())
-	_, err = file.WriteString(knownhosts.Line([]string{knownHosts}, pubKey))
-	return err
-}
+// 	knownHosts := knownhosts.Normalize(remote.String())
+// 	_, err = file.WriteString(knownhosts.Line([]string{knownHosts}, pubKey))
+// 	return err
+// }
 
-func createKnownHosts(user string) string {
-	knownHostsFilepath := fmt.Sprintf("/home/%s/.ssh/known_hosts", user)
-	file, err := os.OpenFile(knownHostsFilepath, os.O_CREATE, 0600)
-	if err != nil {
-		escape.LogPrint(err.Error())
-	}
-	file.Close()
+// func createKnownHosts(user string) string {
+// 	knownHostsFilepath := fmt.Sprintf("/home/%s/.ssh/known_hosts", user)
+// 	file, err := os.OpenFile(knownHostsFilepath, os.O_CREATE, 0600)
+// 	if err != nil {
+// 		escape.LogPrint(err.Error())
+// 	}
+// 	file.Close()
 
-	return knownHostsFilepath
-}
+// 	return knownHostsFilepath
+// }
 
-func checkKnownHosts(user string) ssh.HostKeyCallback {
-	knownHostsFile := createKnownHosts(user)
-	knownHosts, err := knownhosts.New(knownHostsFile)
+// func checkKnownHosts(user string) ssh.HostKeyCallback {
+// 	knownHostsFile := createKnownHosts(user)
+// 	knownHosts, err := knownhosts.New(knownHostsFile)
 
-	if err != nil {
-		escape.LogPrint(err.Error())
-		return nil
-	}
-	return knownHosts
-}
+// 	if err != nil {
+// 		escape.LogPrint(err.Error())
+// 		return nil
+// 	}
+// 	return knownHosts
+// }
 
 func Run(user, server, cmd, prvt_key string) error {
 	cmd = fmt.Sprintf("bash %s", cmd)
 
 	user_ssh_dir := fmt.Sprintf("/home/%s/.ssh", user)
 
-	// allowed_hosts_file := fmt.Sprintf("%s/known_hosts", user_ssh_dir)
-	// publicKeyBytes, _ := os.ReadFile(allowed_hosts_file)
-	// _, hosts, pubkey, _, _, _ := ssh.ParseKnownHosts(publicKeyBytes)
-	// escape.LogPrint(pubkey)
-	// escape.LogPrint(hosts)
-
 	if prvt_key == "" {
 		prvt_key = fmt.Sprintf("%s/id_rsa", user_ssh_dir)
 	}
-	privateKey, err := os.ReadFile(prvt_key)
 
+	privateKey, err := os.ReadFile(prvt_key)
 	if err != nil {
 		return err
 	}
+
 	key, err := ssh.ParsePrivateKey(privateKey)
 
 	if err != nil {
 		return err
 	}
+	knownhostsFile := fmt.Sprintf("%s/known_hosts", user_ssh_dir)
+	hstcallback, err := knownhosts.New(knownhostsFile)
 
-	var keyErr *knownhosts.KeyError
+	if err != nil {
+		return err
+	}
 
 	config := &ssh.ClientConfig{
-		User: user,
-		HostKeyCallback: ssh.HostKeyCallback(func(host string, remote net.Addr, pubKey ssh.PublicKey) error {
-			kh := checkKnownHosts(user)
-
-			err := kh(host, remote, pubKey)
-
-			if errors.As(err, &keyErr) && len(keyErr.Want) > 0 {
-
-				return keyErr
-			} else if errors.As(err, &keyErr) && len(keyErr.Want) == 0 {
-
-				return addHostKey(host, user, remote, pubKey)
-			}
-			return nil
-		}),
+		User:            user,
+		HostKeyCallback: hstcallback,
 		Auth: []ssh.AuthMethod{
 			ssh.PublicKeys(key),
 		},
 	}
+	var client *ssh.Client
 
-	client, err := ssh.Dial("tcp", net.JoinHostPort(server, "22"), config)
-
+	serverIP, err := net.LookupIP(server)
 	if err != nil {
 		return err
+	}
+
+	for _, ip := range serverIP {
+		client, err = ssh.Dial("tcp", net.JoinHostPort(ip.String(), "22"), config)
+		if err == nil {
+			break
+		}
 	}
 
 	session, err := client.NewSession()
@@ -113,7 +104,9 @@ func Run(user, server, cmd, prvt_key string) error {
 	defer session.Close()
 
 	err = session.Run(cmd)
+
 	if err != nil {
+
 		return err
 	}
 	return err
